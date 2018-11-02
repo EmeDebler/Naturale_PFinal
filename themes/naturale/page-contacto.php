85efd03dac724ffbf8cbae2787fb87aeb14e7d60
emelina <deblercita25@mail.com>
2018-11-02 03:20:03 -0300

    se modifica formulario de contacto

--- themes/naturale/page-contacto.php
+++ themes/naturale/page-contacto.php
@@ -1,23 +1,18 @@
 <?php get_header(); ?>
 
-<div class="col-12 col-sm-12 col-md-12 col-lg-12"></div>
-  <div class="container">
-    <div class="row">
-      <div class="col-12 col-sm-12 col-md-6 col-lg-6 text-center"><h1 class="font_title">Ubicación
-       </h1>
+  
+  <div class="col-12 col-sm-12 text-center">
+    <h1 class="font_title">Ubicación</h1>
      <hr color="DCFA57">
-       <div class="col-12 col-sm-12 col-md-6 col-lg-6 text-center">
-            <iframe class="img-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4712.376682621574!2d-70.73038185531385!3d-33.36705816401308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c0ddb38461cf%3A0x97999a3dbdb39e5b!2sMunicipalidad+de+Quilicura!5e0!3m2!1ses-419!2scl!4v1540177296050" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-       </div>
-      </div>
-       <div class="col-12 col-sm-12 col-md-2 col-lg-2"></div>
-      <div class="col-12 col-sm-12 col-md-4 col-lg-4"><h1 class="text-center font_title">Contáctanos
-       </h1>
+     
+   <iframe class="img-fluid" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3332.0808280862634!2d-70.73449348534719!3d-33.36895578079737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c0ddb38461cf%3A0x97999a3dbdb39e5b!2sMunicipalidad+de+Quilicura!5e0!3m2!1ses-419!2scl!4v1541136280481" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      
+   
+       
+   <h1 class="text-center font_title">Contáctanos</h1>
      <hr color="DCFA57">
         <?php echo do_shortcode('[contact-form-7 id="39" title="Formulario de contacto 1"]') ?>
-      </div>
-    </div>
-  </div>
+   </div>
 <!--logo Quilicura-->
   <div class="container">
     <div class="row">
